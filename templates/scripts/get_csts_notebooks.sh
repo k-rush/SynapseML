6e6144aef745792516e88edb57ec08f8de1c8ade
Kyle Rush <kyrush@microsoft.com>
2023-01-06 17:03:27 -0800

    Generate manifest file

--- templates/scripts/get_csts_notebooks.sh
+++ templates/scripts/get_csts_notebooks.sh
@@ -6,10 +6,6 @@ target="cst"
 # Remove all files in the staging directory
 rm $destination*
 
-# Get all Jupyter notebook files in the directory
-
-notebook_files_with_cst_target=()
-
 echo "Finding all notebooks in $source which have a target of $target."
 # Iterate through the notebook files
 while read -r notebook_file; do
@@ -22,12 +18,21 @@ while read -r notebook_file; do
   # Add notebooks to collection if target is specified
   if [[ "$targets" =~ $target ]]
   then
-    notebook_files_with_cst_target+=($notebook_file)
+    echo "Notebook $notebook_file has a target for $target. Moving to staging directory $destination"
     cp "$notebook_file" "$destination"
   fi  
 done < <(find $source -name "*.ipynb")
 
-echo "Notebooks with a '$target' target: ${notebook_files_with_cst_target[@]}"
+#Remove spaces and hyphens from file names in staging directory
+find $destination -type f -name "* *.ipynb" | while read file; do
+  filename=$(basename -- "$file")
+  extension="${filename##*.}"
+  filename="${filename%.*}"
+
+  new_name=$(echo $filename | tr -d " -.")
+  echo "Renaming $file to $new_name.$extension"
+  mv "$file" $destination/"$new_name.$extension"
+done
 
 # Activate the conda environment
 source activate synapseml
@@ -42,17 +47,10 @@ do
 done
 
 # Clean up jupyter notebooks
+echo "Removing all *.ipynb files from staging directory"
 rm $destination*.ipynb
 
-#Remove spaces and hyphens from file names in staging directory
-# Get a list of all files in the directory
-python_files=$(find $staging -name "*.py")
-
-# Iterate through the list of files
-for file in $python_files; do
-  # Replace spaces and hyphens
-  new_name=$(echo "$file" | tr ' -' '_')
-
-  # Rename the file
-  mv "$file" "$new_name"
-done
+# Iterate through the files in the directory
+find $destination -name '*.py' -exec jq -n --arg path {} '{ path: $path }' \; | jq -n -s '{ notebooks: inputs }' > $destination/manifest.json
+echo "Manifest file:"
+cat $destination/manifest.json
