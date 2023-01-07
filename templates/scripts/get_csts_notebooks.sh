ab058613c3cdcc76ad5794cc89f546a5a633aebc
Kyle Rush <kyrush@microsoft.com>
2023-01-06 17:35:53 -0800

    Add pipeline

--- templates/scripts/get_csts_notebooks.sh
+++ templates/scripts/get_csts_notebooks.sh
@@ -1,10 +1,36 @@
-# Set the directory path
-source="./notebooks/features/"
-destination="./staging/"
-target="cst"
+if [ -z "$1" ]
+then
+  source="./notebooks/features"
+else
+  source="$1"
+fi
+
+if [ -z "$2" ]
+then
+  target="cst"
+else
+  target="$2"
+fi
+
+# Check if the BUILD_ARTIFACTSTAGINGDIRECTORY environment variable is set, if not use relative path
+if [ -z "$BUILD_ARTIFACTSTAGINGDIRECTORY" ]
+then
+  staging_dir="./notebook-artifacts-staging/"
+else
+  staging_dir="$BUILD_ARTIFACTSTAGINGDIRECTORY/notebook-artifacts-staging/"
+fi
+
+echo "staging_dir: $staging_dir"
+
+# Check if the directory exists
+if [ ! -d "$staging_dir" ]; then
+  # Create the directory if it does not exist
+  mkdir "$staging_dir"
+fi
+
 
 # Remove all files in the staging directory
-rm $destination*
+rm $staging_dir*
 
 echo "Finding all notebooks in $source which have a target of $target."
 # Iterate through the notebook files
@@ -18,39 +44,36 @@ while read -r notebook_file; do
   # Add notebooks to collection if target is specified
   if [[ "$targets" =~ $target ]]
   then
-    echo "Notebook $notebook_file has a target for $target. Moving to staging directory $destination"
-    cp "$notebook_file" "$destination"
+    echo "Notebook $notebook_file has a target for $target. Moving to staging directory $staging_dir"
+    cp "$notebook_file" "$staging_dir"
   fi  
 done < <(find $source -name "*.ipynb")
 
 #Remove spaces and hyphens from file names in staging directory
-find $destination -type f -name "* *.ipynb" | while read file; do
+find $staging_dir -type f -name "* *.ipynb" | while read file; do
   filename=$(basename -- "$file")
   extension="${filename##*.}"
   filename="${filename%.*}"
 
   new_name=$(echo $filename | tr -d " -.")
   echo "Renaming $file to $new_name.$extension"
-  mv "$file" $destination/"$new_name.$extension"
+  mv "$file" $staging_dir/"$new_name.$extension"
 done
 
 # Activate the conda environment
 source activate synapseml
 
-# Set the directory containing the notebooks
-notebook_dir="./staging"
-
 # Convert each notebook to Python
-for notebook in "$notebook_dir/*.ipynb"
+for notebook in "$staging_dir/*.ipynb"
 do
     jupyter nbconvert --to python "$notebook"
 done
 
 # Clean up jupyter notebooks
 echo "Removing all *.ipynb files from staging directory"
-rm $destination*.ipynb
+rm $staging_dir*.ipynb
 
 # Iterate through the files in the directory
-find $destination -name '*.py' -exec jq -n --arg path {} '{ path: $path }' \; | jq -n -s '{ notebooks: inputs }' > $destination/manifest.json
+find $staging_dir -name '*.py' -exec jq -n --arg path {} '{ path: $path }' \; | jq -n -s '{ notebooks: inputs }' > $staging_dir/manifest.json
 echo "Manifest file:"
-cat $destination/manifest.json
+cat $staging_dir/manifest.json
